d5e5f06292777296b5c510140798a59853c3d88c
n-charan <nandavaramcharan2001@gmail.com>
2025-08-07 23:23:10 +0530

    updated code

--- src/main/java/com/example/spring_sec_demo/model/UserPrinciple.java
+++ src/main/java/com/example/spring_sec_demo/model/UserPrinciple.java
@@ -1,54 +1,54 @@
-package com.example.spring_sec_demo.model;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.Collections;
-
-public class UserPrinciple implements UserDetails {
-
-
-    public Student student;
-
-    public UserPrinciple(Student student) {
-        this.student = student;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(new SimpleGrantedAuthority("Student"));
-    }
-
-    @Override
-    public String getPassword() {
-        return student.getPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return student.getName();
-    }
-
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return UserDetails.super.isAccountNonExpired();
+//package com.example.spring_sec_demo.model;
+//
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.Collection;
+//import java.util.Collections;
+//
+//public class UserPrinciple implements UserDetails {
+//
+//
+//    public Student student;
+//
+//    public UserPrinciple(Student student) {
+//        this.student = student;
 //    }
 //
 //    @Override
-//    public boolean isAccountNonLocked() {
-//        return UserDetails.super.isAccountNonLocked();
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return Collections.singleton(new SimpleGrantedAuthority("Student"));
 //    }
 //
 //    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return UserDetails.super.isCredentialsNonExpired();
+//    public String getPassword() {
+//        return student.getPassword();
 //    }
 //
 //    @Override
-//    public boolean isEnabled() {
-//        return UserDetails.super.isEnabled();
+//    public String getUsername() {
+//        return student.getName();
 //    }
-}
+//
+////    @Override
+////    public boolean isAccountNonExpired() {
+////        return UserDetails.super.isAccountNonExpired();
+////    }
+////
+////    @Override
+////    public boolean isAccountNonLocked() {
+////        return UserDetails.super.isAccountNonLocked();
+////    }
+////
+////    @Override
+////    public boolean isCredentialsNonExpired() {
+////        return UserDetails.super.isCredentialsNonExpired();
+////    }
+////
+////    @Override
+////    public boolean isEnabled() {
+////        return UserDetails.super.isEnabled();
+////    }
+//}
